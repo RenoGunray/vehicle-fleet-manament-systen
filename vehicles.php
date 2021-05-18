22b71f2e73a0037e1c4d3c0622d719f261dfd6ae
RenoGunray <theodorenkhwamba@gmail.com>
2021-05-18 17:05:01 +0200

    changed display code

--- vehicles.php
+++ vehicles.php
@@ -116,6 +116,55 @@ include 'conn.php';
                         } ?>
         </table> <!--table displaying vehicle information-->
     </div><!--end table box starts here-->
+
+    <div class="table-box"> <!--table box starts here-->
+        <div class="table-box-title-bar">
+            <p>Assigned</p>
+            <a href="#" class="cus-btn">Assign vehicle</a>
+        </div>
+        <table class="tables"> <!--table displaying vehicle information-->
+            <!--table headers-->
+            <th>id</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Capacity</th>
+            <th>Driver #</th>
+            <th>Driver</th>
+            <?php $sel = mysqli_query($conn, "select * from vehicles join assigned_vehicles where v_id!=vehicle_id");?>
+
+            <?php
+                if(mysqli_num_rows($sel)==0) {
+                    echo "<p class='text-danger'><strong>There are no vehicles assigned to drivers</strong></p>";
+                }else{
+                    while($row=mysqli_fetch_array($sel)) { //while loop: fetching vehicle information
+                        $v_id = $row['v_id'];
+                        $name = $row['name'];
+                       // $model = $row['model'];
+                       // $serial = $row['serialno'];
+                        $type = $row['type'];
+                        $capacity = $row['capacity'];
+                        //$driver = $row['driver'];
+                        //$driver_id = $row['driver_id'];
+            ?>
+
+            <?php  
+            // $sel2 = mysqli_query($conn, "select * from drivers where dr_id='$driver_id'");
+            // while($row2=mysqli_fetch_array($sel2)){
+            //     $driver_name = $row2['fname'].' '.$row2['lname'];
+            ?>
+            <tr>
+            <td><?php echo $v_id; ?></td>
+            <td><?php echo $name; ?></td>
+            <td><?php echo $type; ?></td>
+            <td><?php echo $capacity; ?></td>
+            <td><?php// echo $driver_id; ?></td>
+            <td><?php// echo $driver_name; ?></td>
+            </tr>
+            <?php }//end while loop: fetching vehicle information
+                    }//end else statement  
+                     ?>
+        </table> <!--table displaying vehicle information-->
+    </div><!--end table box starts here-->
     </div>
 </div>
 <!--END MAIN CONTAINER:: ALL THINGS CONTAINED-->
